132ee25be2910a3d8629ad08d857eb1a74408247
sinelnikovd <webxrab@gmail.com>
2016-01-25 15:04:44 +0600

    first

---
+++ app/js/common.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+
+	if($('input.data-inputmask').length > 0) {
+		$('input.data-inputmask').mask("+7 (999) 999-99-99");
+	}
+
+
+	$('.cart').magnificPopup({
+		items: {
+			src: '.cart-popup',
+			type: 'inline'
+		}
+	});
+
+	$('.cart-popup__link').magnificPopup({
+		items: {
+			src: '.decor-popup',
+			type: 'inline'
+		}
+	});
+
+	$('.product-card__link').magnificPopup({
+		items: {
+			src: '.product-popup',
+			type: 'inline'
+		}
+	});
+
+	var carouselCustomer = $('.product-carousel__items').owlCarousel({
+		loop: true,
+		nav: false,
+		items: 1
+	})
+	$('.product-carousel__prev').click(function() {
+		carouselCustomer.trigger('prev.owl.carousel');
+	})
+	$('.product-carousel__next').click(function() {
+		carouselCustomer.trigger('next.owl.carousel');
+	})
+
+	 $(".product-cards-slider").each(function(i, el) {
+		$(el).children(".product-cards-slider__overview").sly({
+			horizontal: 1,
+			itemNav: 'basic',
+			mouseDragging: 1,
+			touchDragging: 1,
+			releaseSwing: 1,
+			scrollBar: $(el).children('.product-cards-slider__scrollbar'),
+			speed: 300,
+			dragHandle: 1,
+		});
+	});
+	$(window).resize(function(e) {
+		$(".product-cards-slider").each(function(i, el) {
+			$(el).children(".product-cards-slider__overview").sly('reload');
+		});
+	});
+
+
+});
+
+	//SVG Fallback
+if (!Modernizr.svg) {
+	// wrap this in a closure to not expose any conflicts
+	(function() {
+		// grab all images. getElementsByTagName works with IE5.5 and up
+		var imgs = document.getElementsByTagName('img'),endsWithDotSvg = /.*\.svg$/,i = 0,l = imgs.length;
+		// quick for loop
+		for(; i < l; ++i) {
+			if(imgs[i].src.match(endsWithDotSvg)) {
+				// replace the png suffix with the svg one
+				imgs[i].src = imgs[i].src.slice(0, -3) + 'png';
+			}
+		}
+	})();
+}
